041cd4d19b43678607e6fa3ae407ecc156c3b375
hung1094 <hoangmanhhung05052005@gamil.com>
2026-04-01 13:20:22 +0700

    feat: configure docker, jdk 25 and database connected

--- websitebanmatkinh/src/main/java/phattrienungdungj2ee/websitebanmatkinh/WebsitebanmatkinhApplication.java
+++ websitebanmatkinh/src/main/java/phattrienungdungj2ee/websitebanmatkinh/WebsitebanmatkinhApplication.java
@@ -1,7 +1,12 @@
 package phattrienungdungj2ee.websitebanmatkinh;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+// Nhớ thay đổi đường dẫn package bên dưới cho đúng với project của bạn nếu bị báo đỏ
+import phattrienungdungj2ee.websitebanmatkinh.repository.UserRepository;
+import phattrienungdungj2ee.websitebanmatkinh.entity.User;
 
 @SpringBootApplication
 public class WebsitebanmatkinhApplication {
@@ -10,4 +15,19 @@ public class WebsitebanmatkinhApplication {
 		SpringApplication.run(WebsitebanmatkinhApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner upgradeToAdmin(UserRepository userRepository) {
+		return args -> {
+			// CHÚ Ý: Thay tên tài khoản bạn đã đăng ký vào đây
+			String myUsername = "hung";
+
+			userRepository.findByUsername(myUsername).ifPresent(user -> {
+				user.setRole("ADMIN");
+				userRepository.save(user);
+				System.out.println("----------------------------------------------");
+				System.out.println("✅ THÀNH CÔNG: " + myUsername + " ĐÃ LÀ ADMIN!");
+				System.out.println("----------------------------------------------");
+			});
+		};
+	}
 }
